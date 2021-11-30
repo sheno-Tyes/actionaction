823dfa587302cd7e9e4fae2720cb010d9cedfec3
sheno-Tyes <42202881+sheno-Tyes@users.noreply.github.com>
2021-11-30 11:50:45 +0800

    Update diy-x86-64-li-2.sh

--- diy-x86-64-li-2.sh
+++ diy-x86-64-li-2.sh
@@ -15,11 +15,8 @@
 #pushd package
 #git clone --depth=1 https://github.com/fw876/helloworld
 #popd
-echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
-echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
-./scripts/feeds clean
-./scripts/feeds update -a
-./scripts/feeds install -a
+
+
 mkdir package/community
 pushd package/community
 # Add Lienol's Packages
