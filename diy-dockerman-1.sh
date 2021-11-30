774f513213c2bf19fea0bf2ebf49fa3d306a513f
sheno-Tyes <42202881+sheno-Tyes@users.noreply.github.com>
2021-11-30 18:23:34 +0800

    Update diy-dockerman-1.sh

--- diy-dockerman-1.sh
+++ diy-dockerman-1.sh
@@ -8,6 +8,5 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-sed -i "/other/d" feeds.conf.default
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
