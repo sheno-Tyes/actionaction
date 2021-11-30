dfc98166adc9d248c6859677ece20a267819bdc9
sheno-Tyes <42202881+sheno-Tyes@users.noreply.github.com>
2021-11-30 12:28:25 +0800

    Update diy-x86-64-li-2.sh

--- diy-x86-64-li-2.sh
+++ diy-x86-64-li-2.sh
@@ -29,7 +29,9 @@ git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus
 # Add luci-app-vssr <M>
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
 git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
-
+#add docker
+git clone https://github.com/lisaac/luci-lib-docker
+git clone https://github.com/lisaac/luci-app-dockerman
 
 
 popd
