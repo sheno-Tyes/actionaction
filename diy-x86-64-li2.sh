fa24e2ff97fa9b1b4e084bfdda80ee7e1508c329
sheno-Tyes <42202881+sheno-Tyes@users.noreply.github.com>
2022-01-11 23:55:59 +0800

    Update diy-x86-64-li2.sh

--- diy-x86-64-li2.sh
+++ diy-x86-64-li2.sh
@@ -18,8 +18,10 @@
 rm -rf ./feeds/other/luci-lib-docker
 rm -rf ./package/feeds/other/luci-lib-docker
 rm -rf ./feeds/luci/collections/luci-lib-docker
+rm -rf ./package/feeds/luci/luci-lib-docker
 rm -rf ./feeds/luci/applications/luci-app-dockerman
 rm -rf ./feeds/other/luci-app-dockerman
+rm -rf ./package/feeds/luci/luci-app-dockerman
 ##
 rm -rf ./package/feeds/other/luci-app-adguardhome
 rm -rf ./feeds/other/luci-app-adguardhome
